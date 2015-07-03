af0765f5c3734f9b6415f0d04212c48b5cab246b
Xudong Liu <kingliuice@gmail.com>
2015-07-03 17:50:21 +0800

    new

--- controllers/default.go
+++ controllers/default.go
@@ -28,8 +28,8 @@ func TextMessageHandler(w http.ResponseWriter, r *mp.Request) {
 	// 简单起见，把用户发送过来的文本原样回复过去
 	text := request.GetText(r.MixedMsg) // 可以省略, 直接从 r.MixedMsg 取值
 	resp := response.NewText(text.FromUserName, text.ToUserName, text.CreateTime, text.Content)
-	//mp.WriteRawResponse(w, r, resp) // 明文模式
-	mp.WriteAESResponse(w, r, resp) // 安全模式
+	mp.WriteRawResponse(w, r, resp) // 明文模式
+	//mp.WriteAESResponse(w, r, resp) // 安全模式
 }
 
 func (c *MainController) Get() {
@@ -47,6 +47,7 @@ func (c *MainController) Get() {
 
 func (c *MainController) Post() {
 
+	Log.Info("Post Request")
 	aesKey, err := util.AESKeyDecode("6c0p5ooqHpxLm8tDQo2yFN0lPl8FiunHz6s3uwBHMyj") // 这里 encodedAESKey 改成你自己的参数
 	if err != nil {
 		panic(err)
